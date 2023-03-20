6371a071efc1209fb8f9008cd57f44d5174e9a55
Victor Bondarev <125218061+Victor-Bondarev@users.noreply.github.com>
2023-03-20 20:54:34 +0300

    b103

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,55 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double s = 1;
+    for (uint16_t i = 0; i < n; ++i) {
+        s *= value;
+    }
+    return s;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n <= 1)
+        return 1;
+    else
+        return fact(n - 1) * n;
 }
 
 double calcItem(double x, uint16_t n) {
 
+    double num = 1, den = 1;
+    for (uint16_t i = 1; i <= n; ++i) {
+        num *= x;
+        den *= i;
+    }
+    return num / den;
 }
 
 double expn(double x, uint16_t count) {
 
+    double s = 1, c = 1;
+    for (uint16_t i = 1; i <= count; ++i) {
+        c *= x / i;
+        s += c;
+    }
+    return s;
 }
 
 double sinn(double x, uint16_t count) {
 
+    double s = x, c = x;
+    for (uint16_t i = 1; i <= count; ++i) {
+        c *= -1 * x * x / (2 * i) / (2 * i + 1);
+        s += c;
+    }
+    return s;
 }
 
 double cosn(double x, uint16_t count) {
 
+    double c = 1.0;
+    for (int i = 2; i <= count; ++i) {
+        c += pown(-1, i - 1) * calcItem(x, i * 2 - 2);
+    }
+    return c;
 }
